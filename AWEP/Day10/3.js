5e2a4721962101e4fa36051c003a1323d5749513
snehasomwanshi7 <snehasom123@gmail.com>
2020-08-17 20:01:44 +0530

    AWEP Assignments

---
+++ AWEP/Day10/3.js
@@ -0,0 +1,12 @@
+let likeMe = function (btnElement) {
+    // Innerhtml returs STRING.
+    let likeCountString = btnElement.children[0].innerHTML;
+
+    // string to number
+    let likeCount = parseInt(likeCountString);
+
+    // lets increase the like count
+    likeCount++;
+
+    btnElement.children[0].innerHTML = likeCount;
+};
